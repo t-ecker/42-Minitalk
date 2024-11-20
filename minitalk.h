a30f02ac604518741733619631e2b3d40da0a9a9
t-ecker <152566019+t-ecker@users.noreply.github.com>
2024-11-20 19:05:28 +0100

    cleanup

--- minitalk.h
+++ minitalk.h
@@ -6,14 +6,14 @@
 /*   By: tomecker <tomecker@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 19:52:04 by tomecker          #+#    #+#             */
-/*   Updated: 2024/04/25 21:23:23 by tomecker         ###   ########.fr       */
+/*   Updated: 2024/11/20 19:04:42 by tomecker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include "Libft/libft.h"
+# include "libft/libft.h"
 # include <unistd.h>
 # include <signal.h>
 # include <stdlib.h>
